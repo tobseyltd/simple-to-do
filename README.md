5f00eedd947459dd22468a521d72ac2c41999822
tobseyltd <mail@tobsey.limited>
2023-02-10 08:15:45 +0100

    commit (#6)

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ### Creating a To Do App
 
 A to do App that let users create / delete tasks and mark / unmark them as done / undone.
+Please feel free to comment and give feedback on the written html, css and js code.
 
 >LIVE VERSION: https://tobseyltd.github.io/simple-to-do/
--- js/script.js
+++ js/script.js
@@ -9,42 +9,44 @@ document.getElementById('submit').onclick = () => {
     else {
         document.getElementById('task-board').innerHTML +=
             `<div id="task" class="flex-inline">
-                         <input type="checkbox" id="checkbox">
-                         <p id="task_open">${document.querySelector('.task-input input').value}</p>       
-                         <img id="delete-button" src="./media/delete.png"/>
-            </div>`;
-
+                <input type="checkbox" id="checkbox">
+                <p id="task_open">${document.querySelector('.task-input input').value}</p>       
+                <img id="delete-button" src="./media/delete.png"/>
+            </div>`
 
         document.querySelectorAll('#delete-button').forEach((deleteButton) => {
             deleteButton.onclick = () => {
                 deleteButton.parentNode.remove();
-            }
-        })
-
-        const checkbox_nodeList = document.querySelectorAll('#checkbox');
-        const task_nodeList = document.querySelectorAll('#task_open');
-
-        for (j = 0; j < task_nodeList.length; j++) {
-
-            for (let i = j; i < checkbox_nodeList.length; i++) {
-                checkbox_nodeList[i].onclick = () => {
-
-                    if (checkbox_nodeList[i].checked) {
-                        task_nodeList[i].style.textDecoration = 'line-through';
-                    }
-                    else {
-                        task_nodeList[i].style.textDecoration = 'none';
-                    }
-                }
-            }
-        }
+            };
+        });
     };
+
+    const checkbox_nodeList = document.querySelectorAll('#checkbox');
+    const task_nodeList = document.querySelectorAll('#task_open');
+
+    for (j = 0; j < task_nodeList.length; j++) {
+
+        for (let i = j; i < checkbox_nodeList.length; i++) {
+            checkbox_nodeList[i].onclick = () => {
+
+                if (checkbox_nodeList[i].checked) {
+                    task_nodeList[i].style.textDecoration = 'line-through';
+                }
+                else {
+                    task_nodeList[i].style.textDecoration = 'none';
+                };
+            };
+        };
+    };
+
     document.querySelector('.task-input input').value = '';
-}
+
+};
 
 
 document.getElementById('reset').onclick = () => {
-    document.querySelector('.task-input input').value = '';
-}
+        document.querySelectorAll('#task').forEach((task) => {
+            task.remove();
+        });
 
-
+    };
